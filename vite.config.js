2053f75b5a6bf2a7746af70f9feec4e2fa6275da
Francisco Agustin Muñoz <franmunoz2501@gmail.com>
2026-03-31 11:05:50 -0300

    Changes in route

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' // <--- Importa esto
 
 export default defineConfig({
+  base: '/proyectoobrasepec/',
   plugins: [
     react(),
     tailwindcss(), // <--- Agrégalo aquí
